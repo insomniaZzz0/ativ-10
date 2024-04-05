767845893a1b57a32618ed4729f6de7fd265292b
insomniaZzz0 <160507585+insomniaZzz0@users.noreply.github.com>
2024-04-05 09:05:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+     
+     $servidor="localhost";
+     $usuario="escola";
+     $senha="123";
+     $nomedodb="escola";
+
+     $conn=new mysqli($servidor, $usuario, $senha, $nomedodb);
+
+     if ($conn->connect_error){
+         die("Conexâo Falhou".$conn->connect_error);
+    }
+
+    echo"Conectado ao DB";
+
+?>
